8f44dde8b6062a78c7ee59bcfc7da21cac6e8c29
Олег-Афонин <routtte@mail.ru>
2021-06-20 20:38:26 +0300

    Устранены замечания ESLint

--- js/frames.js
+++ js/frames.js
@@ -12,17 +12,17 @@ const similarPictures = createDescriptions(25);
 
 // Для каждой фотографии вставляется элемент из шаблона
 const createElements = function(){
-similarPictures.forEach(({url, likes, comments}) => {
-  const newElement = picture.cloneNode(true);
+  similarPictures.forEach(({url, likes, comments}) => {
+    const newElement = picture.cloneNode(true);
 
-  newElement.querySelector('.picture__img').src = url;
-  newElement.querySelector('.picture__likes').textContent = likes;
-  newElement.querySelector('.picture__comments').textContent = comments.length;
+    newElement.querySelector('.picture__img').src = url;
+    newElement.querySelector('.picture__likes').textContent = likes;
+    newElement.querySelector('.picture__comments').textContent = comments.length;
 
-  picturesListFragment.appendChild(newElement);
-});
+    picturesListFragment.appendChild(newElement);
+  });
 
- picturesList.appendChild(picturesListFragment);
+  picturesList.appendChild(picturesListFragment);
 };
 
 export {createElements};
